c282e4560b116a4eccc912aace03aaca19a4c28e
Toufiq A. Shishir <toufiq_a24@outlook.com>
2024-02-20 01:23:17 +0600

    lab-05 form validation task cont.

--- lab-05/Task_01.php
+++ lab-05/Task_01.php
@@ -1,50 +1,56 @@
 <?php
-    $usrname = $passwd = "";
-    $remember = $_POST["remember"];
+$usrname = $passwd = "";
+$remember = "";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $usrname = validateUsername($_POST["username"]);
-        $passwd = validatePasswd($_POST["password"]);
-        if ($usrname == NULL || $passwd == NULL) echo "Invalid username or password!";
-        else echo "Username: {$usrname} <br> Password: {$passwd} <br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usrname = $_POST["username"];
+    $passwd = $_POST["password"];
+    $remember = ($_POST["remember"] === null) ? $_POST["remember"] : "";
+
+    if (validateUsername($usrname) && validatePassword($passwd)) {
+        echo "Login successful, $usrname <br>";
+    } else {
+        echo "Invalid username or password!<br>";
     }
-    
-    function validateUsername($usrname) {
-        $usrname = trim($usrname);
-        $len = strlen($usrname);
-        if ($len < 2) {
-            return NULL;
-        } else {
-            for ($i = 0; $i < $len; $i++) {
-                $ch = ord($usrname[$i]);
-                if (!($ch >= 48 && $ch <= 57) ||
-                    !($ch >= 65 && $ch <= 90) ||
-                    !($ch >= 97 && $ch <= 122)||
-                    !($usrname[$i] == '.') || !($usrname[$i] == '-') || !($usrname[$i] == '_')) {
-                    return NULL;
-                }
+}
+
+function validateUsername($usrname) {
+    $isValid = true;
+    $usrname = trim($usrname);
+    $len = strlen($usrname);
+    if ($len < 2) {
+        $isValid = false;
+    } else {
+        for ($i = 0; $i < $len; $i++) {
+            $ch = $usrname[$i];
+            if (!(($ch >= 'A' && $ch <= 'Z') ||
+                ($ch >= 'a' && $ch <= 'z') ||
+                ($ch >= '0' && $ch <= '9') ||
+                ($usrname[$i] == '.') || ($usrname[$i] == '-') || ($usrname[$i] == '_'))) {
+                $isValid = false;
+                break;
             }
         }
-        return $usrname;
     }
-    function validatePasswd($passwd) {
-        $passwd = trim($passwd);
-        $len = strlen($passwd);
-        if ($len < 8) {
-            return NULL;
-        } else {
-            for ($i = 0; $i < $len; $i++) {
-                $ch = ord($passwd[$i]);
-                if (!($ch >= 48 && $ch <= 57) && !($ch >= 65 && $ch <= 90) &&
-                    !($ch >= 97 && $ch <= 122)) {
-                    if ($passwd[$i] == '@' || $passwd[$i] = '#' || 
-                        $passwd[$i] = '$' || $passwd[$i] == '%') {
-                    } else {
-                        return NULL;
-                    }
-                }
+    return $isValid;
+}
+
+function validatePassword($passwd) {
+    $isValid = true;
+    $passwd = trim($passwd);
+    $len = strlen($passwd);
+    if ($len < 8) {
+        $isValid = false;
+    } else {
+        $containsSpecialChar = false;
+        for ($i = 0; $i < $len; $i++) {
+            if ($passwd[$i] == '@' || $passwd[$i] == '#' || $passwd[$i] == '$' || $passwd[$i] == '%') {
+                $containsSpecialChar = true;
+                break;
             }
         }
-        return $passwd;
+        if (!$containsSpecialChar) $isValid = false;
     }
+    return $isValid;
+}
 ?>
--- lab-05/Task_02.php
+++ lab-05/Task_02.php
@@ -7,33 +7,35 @@
         $newPasswd = $_POST["new-passwd"];
         $reNewPasswd = $_POST["re-new-passwd"];
 
-        if ($curPasswd == $newPasswd) {
-            echo "New password matches the current password, enter a new password!";
-        } elseif ($newPasswd !== $reNewPasswd) {
-            echo "Retype new password correctly!";
+        if (validatePassword($curPasswd) && validatePassword($newPasswd) && validatePassword($reNewPasswd)) {
+            if ($curPasswd == $newPasswd) {
+                echo "New password matches the current password, enter a new password!";
+            } elseif ($newPasswd !== $reNewPasswd) {
+                echo "Retype new password correctly!";
+            } else {
+                echo "Password change successfull!";
+            }
         } else {
-            echo "Password change successfull!";
+            echo "Invalid password, enter correct password! <br>";
         }
     }
 
-    // function validatePasswd($passwd) {
-    //     $passwd = trim($passwd);
-    //     $len = strlen($passwd);
-    //     if ($len < 8) {
-    //         return NULL;
-    //     } else {
-    //         for ($i = 0; $i < $len; $i++) {
-    //             $ch = ord($passwd[$i]);
-    //             if (!($ch >= 48 && $ch <= 57) && !($ch >= 65 && $ch <= 90) &&
-    //                 !($ch >= 97 && $ch <= 122)) {
-    //                 if ($passwd[$i] == '@' || $passwd[$i] = '#' || 
-    //                     $passwd[$i] = '$' || $passwd[$i] == '%') {
-    //                 } else {
-    //                     return NULL;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return $passwd;
-    // }
+    function validatePassword($passwd) {
+        $isValid = true;
+        $passwd = trim($passwd);
+        $len = strlen($passwd);
+        if ($len < 8) {
+            $isValid = false;
+        } else {
+            $containsSpecialChar = false;
+            for ($i = 0; $i < $len; $i++) {
+                if ($passwd[$i] == '@' || $passwd[$i] == '#' || $passwd[$i] == '$' || $passwd[$i] == '%') {
+                    $containsSpecialChar = true;
+                    break;
+                }
+            }
+            if (!$containsSpecialChar) $isValid = false;
+        }
+        return $isValid;
+    }
 ?>
