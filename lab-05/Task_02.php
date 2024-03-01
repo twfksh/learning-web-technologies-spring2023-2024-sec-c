387bb2d94dba7541e97c2625d8bd04a5a24985f3
Toufiq A. Shishir <toufiq_a24@outlook.com>
2024-03-01 11:47:40 +0600

    lab-05 cont. custom str func

--- lab-05/Task_02.php
+++ lab-05/Task_02.php
@@ -22,8 +22,8 @@
 
     function validatePassword($passwd) {
         $isValid = true;
-        $passwd = trim($passwd);
-        $len = strlen($passwd);
+        $passwd = custom_trim($passwd);
+        $len = custom_strlen($passwd);
         if ($len < 8) {
             $isValid = false;
         } else {
@@ -37,5 +37,39 @@
             if (!$containsSpecialChar) $isValid = false;
         }
         return $isValid;
+    }
+
+    function custom_strlen($str) {
+        $len = 0;
+        while (isset($str[$len++]));
+        return $len;
+    }
+    
+    function custom_substr($str, $start, $length = null) {
+        $result = '';
+    
+        $strLen = custom_strlen($str);
+    
+        if ($start < 0) $start = max(0, $strLen + $start);
+    
+        if ($length !== null && $length < 0) $length = max(0, $strLen + $length - $start);
+    
+        for ($i = $start; $i < $strLen && ($length === null || $i < $start + $length); $i++) $result .= $str[$i];
+    
+        return $result;
+    }
+    
+    function custom_trim($str) {
+        $len = custom_strlen($str);
+        if ($len == 0) return $str;
+    
+        $begin = 0;
+        $end = $len - 1;
+    
+        while ($begin < $len && $str[$begin] === ' ') $begin++;
+        while ($end >= 0 && $str[$end] === ' ') $end--;
+    
+        if ($begin <= $end) return custom_substr($str, $begin, $end - $begin + 1);
+        else return "";
     }
 ?>
